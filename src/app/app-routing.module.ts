32f6330b7c8167862e87dbf49797edcdc098dcfb
Kumar <ts-pratik.kumar@rakuten.com>
2020-01-06 11:50:13 +0530

    angular-chart.js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './components/radar-chart/radar-chart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bar-chart', component: BarChartComponent},
+  {path: 'pie-chart', component: PieChartComponent},
+  {path: 'doughnut-chart', component: DoughnutChartComponent},
+  {path: 'radar-chart', component: RadarChartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartsModule } from 'ng2-charts';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './components/radar-chart/radar-chart.component';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
+    DoughnutChartComponent,
+    RadarChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
